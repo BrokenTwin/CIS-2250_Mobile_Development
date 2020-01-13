1fb524a71025f708f272bc6680cf565f2eb3875a
Marc <48297737+BrokenTwin@users.noreply.github.com>
2020-01-13 12:15:54 -0400

    Initial commit

---
+++ LayoutApp/app/src/main/java/info/hccis/layoutpresentation/ui/send/SendViewModel.java
@@ -0,0 +1,19 @@
+package info.hccis.layoutpresentation.ui.send;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SendViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SendViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is send fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
